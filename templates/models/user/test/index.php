176b60921705bb64dc741078e40db07806fe91d1
Abdullah Daud <chelahmy@gmail.com>
2019-10-28 21:45:51 +0800

    Added a generalized test function

--- templates/models/user/test/index.php
+++ templates/models/user/test/index.php
@@ -13,7 +13,7 @@ require '../../../../harubi/harubi.php';
 harubi();
 
 $test_auto_num = 0;
-$tests_expected = 5;
+$tests_expected = 7;
 
 function echo_br($str)
 {
@@ -27,47 +27,191 @@ function print_pre($arr)
 	echo "</pre>";
 }
 
+function failed($msg)
+{
+	return "<font color='red'>Failed:</font> $msg<br/>";
+}
+
+function success($msg)
+{
+	echo "<font color='green'>Success:</font> $msg<br/>";
+}
+
+function notice($msg)
+{
+	echo "<font color='orange'>Notice:</font> $msg<br/>";
+}
+
 function as_array($json)
 {
 	return json_decode($json, TRUE);
 }
 
-function with_status($results, $matching)
+function with_status($response, $matching)
 {
-	if (!is_array($results) || !is_array($matching))
+	if (!is_array($response) || !is_array($matching))
 		return FALSE;
 		
-	if (!isset($results['status']) || !isset($matching['status']))
+	if (!isset($response['status']) || !isset($matching['status']))
 		return FALSE;
 		
-	if ($results['status'] == $matching['status'])
+	if ($response['status'] == $matching['status'])
 	{
-		if (!isset($results['error_code']))
+		if (!isset($response['error_code']))
 			return TRUE;
 			
-		if (isset($matching['error_code']) && $matching['error_code'] == $results['error_code'])
+		if (isset($matching['error_code']) && $matching['error_code'] == $response['error_code'])
 			return TRUE;
 	}	
 		
 	return FALSE;		
 }
 
-function testing($line = 0, $msg = '')
+function with_results($response, $matching)
+{
+	if (!is_array($response) || count($response) <= 0 || !isset($response['results']))
+		return TRUE;
+		
+	if (!is_array($matching) || count($matching) <= 0)
+		return TRUE;
+	
+	$results = $response['results'];
+	
+	foreach ($matching as $name => $val)
+	{
+		if (!isset($results[$name]) || $results[$name] != $val)
+			return FALSE;
+	}	
+		
+	return TRUE;		
+}
+
+function testing($line = 0, $title, $msg = '')
 {
 	global $test_auto_num;
 	++$test_auto_num;
 	
-	echo "<br/><font color='blue'>Testing #$test_auto_num</font> [line $line]: $msg<br/>";
+	echo "<br/><font color='blue'>Testing #$test_auto_num</font> [line $line] - <strong>$title</strong> $msg<br/>";
 }
 
-function failed($msg)
+function msg($msg, $ctrl)
 {
-	return "<font color='red'>Failed:</font> $msg";
+	if (!is_array($ctrl) || count($ctrl) <= 0)
+		return $msg;
+		
+	foreach ($ctrl as $name => $val)
+		$msg = str_replace("%$name%", $val, $msg);
+	
+	return $msg;
 }
 
-function success($msg)
+/**
+* Invoke a basic harubi test case.
+* A parameterized request to a test module will be invoked,
+* and the response will be gauged with the expected results.
+* Messages will be echoed.
+* Test $case arguments:
+* [
+*   'line' => 'line number', // use __LINE__
+*   'module' => 'php module file name',
+*   'model' => 'model name',
+*   'action' => 'action name',
+*   'controller' => [
+*     ... controller arguments
+*   ],
+*   'expectation' => [
+*      'status' => 'status code',
+*      'error_code' => 'error code',
+*      'results' => 'results'
+*   ],
+*   'comment' => 'test comment',
+*   'messages' => [
+*     'starting' => 'starting message prior request',
+*     'success' => 'success message',
+*     'failed' => 'failed message'
+*   ]
+* ]
+*/
+function test($case)
 {
-	echo "<font color='green'>Success:</font> $msg<br/>";
+	if (!is_array($case) || count($case) <= 0)
+		die("No test case defined");
+		
+	if (!isset($case['module']))
+		die("Test case: no module defined");
+		
+	if (!isset($case['model']))
+		die("Test case: no model defined");
+		
+	if (!isset($case['action']))
+		die("Test case: no action defined");
+			
+	if (!isset($case['expectation']))
+		die("Test case: no expectation defined");
+
+	$exp = $case['expectation'];
+	
+	if (!isset($exp['status']))
+		die("Test case: no status expection defined");
+	
+	if (isset($exp['results']))
+		$exp_results = $exp['results'];
+	else
+		$exp_results = [];
+			
+	$module = $case['module'];
+	$model  = $case['model'];
+	$action = $case['action'];
+
+	testing($case['line'], "$model::$action", $case['comment']);
+
+	if (isset($case['controller']))
+		$ctrl = $case['controller'];
+	else
+		$ctrl = [];
+
+	if (isset($case['messages']))
+	{
+		$msgs = $case['messages'];
+		
+		if (isset($msgs['starting']))
+			$starting_msg = $msgs['starting'];
+		
+		if (isset($msgs['success']))
+			$success_msg = $msgs['success'];
+		
+		if (isset($msgs['failed']))
+			$failed_msg = $msgs['failed'];
+	}
+	
+	if (isset($starting_msg))
+		echo_br(msg($starting_msg, $ctrl));
+	
+	$response = as_array(request($module, $model, $action, $ctrl));
+	print_pre($response);
+
+	if (!with_status($response, $exp) || !with_results($response, $exp_results))
+	{
+		if (isset($failed_msg))
+			die(failed(msg($failed_msg, $ctrl)));
+		else
+			die(failed("$model::$action"));
+	}
+	
+	global $harubi_last_preset_invoked;
+	
+	if ($harubi_last_preset_invoked !== FALSE)
+		notice("preset <strong>$harubi_last_preset_invoked</strong> was applied");
+
+	global $harubi_last_toll_invoked;
+	
+	if ($harubi_last_toll_invoked !== FALSE)
+		notice("toll <strong>$harubi_last_toll_invoked</strong> was applied");
+
+	if (isset($success_msg))
+		success(msg($success_msg, $ctrl));
+	else
+		success("$model::$action");
 }
 
 /**
@@ -137,7 +281,11 @@ prepare_table($dbname, 'user', '../user.sql');
 session_start();
 
 //--------------------------------------------------------
-testing(__LINE__, "user::signup #1 first user/super-user");
+$module = '../user.php';
+$model  = 'user';
+$action = 'signup';
+
+testing(__LINE__, "$model::$action", "first user/super-user");
 
 $user		= 'admin';
 $password	= 'secret';
@@ -145,7 +293,7 @@ $email		= 'admin@example.com';
 
 echo_br("Signing-up super-user <strong>$user</strong>...");
 $_SESSION['last_reg'] = 0; // bypass sign-up delay
-$results = as_array(request('../user.php', 'user', 'signup', ['name' => $user, 'password' => $password, 'email' => $email]));
+$results = as_array(request($module, $model, $action, ['name' => $user, 'password' => $password, 'email' => $email]));
 print_pre($results);
 
 if (!with_status($results, ['status' => 1]))
@@ -154,7 +302,11 @@ if (!with_status($results, ['status' => 1]))
 success("signed-up super-user <strong>$user</strong>");
 
 //--------------------------------------------------------
-testing(__LINE__, "user::signup #2");
+$module = '../user.php';
+$model  = 'user';
+$action = 'signup';
+
+testing(__LINE__, "$model::$action");
 
 $user		= 'jamal';
 $password	= 'vision';
@@ -162,7 +314,7 @@ $email		= 'jamal@example.com';
 
 echo_br("Signing-up new user <strong>$user</strong>...");
 $_SESSION['last_reg'] = 0; // bypass sign-up delay
-$results = as_array(request('../user.php', 'user', 'signup', ['name' => $user, 'password' => $password, 'email' => $email]));
+$results = as_array(request($module, $model, $action, ['name' => $user, 'password' => $password, 'email' => $email]));
 print_pre($results);
 
 if (!with_status($results, ['status' => 1]))
@@ -171,13 +323,17 @@ if (!with_status($results, ['status' => 1]))
 success("signed-up new user <strong>$user</strong>");
 
 //--------------------------------------------------------
-testing(__LINE__, "user::signin non-user");
+$module = '../user.php';
+$model  = 'user';
+$action = 'signin';
+
+testing(__LINE__, "$model::$action", "non-user");
 
 $user		= 'ali';
 $password	= 'wisdom';
 
 echo_br("Signing-in non-user <strong>$user</strong>...");
-$results = as_array(request('../user.php', 'user', 'signin', ['name' => $user, 'password' => $password]));
+$results = as_array(request($module, $model, $action, ['name' => $user, 'password' => $password]));
 print_pre($results);
 
 if (!with_status($results, ['status' => 0, 'error_code' => 1]))
@@ -185,14 +341,150 @@ if (!with_status($results, ['status' => 0, 'error_code' => 1]))
 
 success("signing-in non-user <strong>$user</strong> failed as expected");
 
+
 //--------------------------------------------------------
-testing(__LINE__, "user::signin existing user");
+test([
+	'line' => __LINE__,
+	'module' => '../user.php',
+	'model' => 'user',
+	'action' => 'signin',
+	'controller' => [
+		'user' => 'abu',
+		'password' => 'morning'
+	],
+	'expectation' => [
+		'status' => 0,
+		'error_code' => 1
+	],
+	'comment' => 'non-user',
+	'messages' => [
+		'starting' => 'Signing-in non-user <strong>%user%</strong>...',
+		'success' => 'signing-in non-user <strong>%user%</strong> failed as expected',
+		'failes' => 'allowing non-user <strong>%user%</strong> to sign-in'
+	]
+]);
+
+//--------------------------------------------------------
+$module = '../user.php';
+$model  = 'user';
+$action = 'read_own';
+
+testing(__LINE__, "$model::$action", "without signing-in");
+
+$user		= 'jamal';
+
+echo_br("Reading user <strong>$user</strong> own record without signing-in...");
+$results = as_array(request($module, $model, $action));
+print_pre($results);
+
+if (!with_status($results, ['status' => 0, 'error_code' => 1000]))
+	die(failed("allowing to read user <strong>$user</strong> own record without signing-in"));
+
+if ($harubi_last_preset_invoked !== FALSE)
+	notice("preset <strong>$harubi_last_preset_invoked</strong> was applied");
+
+success("reading user <strong>$user</strong> own record without signing-in failed as expected");
+
+//--------------------------------------------------------
+$module = '../user.php';
+$model  = 'user';
+$action = 'update_own';
+
+testing(__LINE__, "$model::$action", "without signing-in");
+
+$user		= 'jamal';
+$password	= 'vision1';
+$email		= 'jamal_one@example.com';
+
+echo_br("Updating user <strong>$user</strong> own record without signing-in...");
+$results = as_array(request($module, $model, $action, ['password' => $password, 'email' => $email]));
+print_pre($results);
+
+if (!with_status($results, ['status' => 0, 'error_code' => 1000]))
+	die(failed("allowing to update user <strong>$user</strong> own record without signing-in"));
+
+if ($harubi_last_preset_invoked !== FALSE)
+	notice("preset <strong>$harubi_last_preset_invoked</strong> was applied");
+
+success("updating user <strong>$user</strong> own record without signing-in failed as expected");
+
+//--------------------------------------------------------
+$module = '../user.php';
+$model  = 'user';
+$action = 'read';
+
+testing(__LINE__, "$model::$action", "without signing-in");
+
+$user		= 'jamal';
+
+echo_br("Reading user <strong>$user</strong> record without signing-in...");
+$results = as_array(request($module, $model, $action, ['user' => $user]));
+print_pre($results);
+
+if (!with_status($results, ['status' => 0, 'error_code' => 1000]))
+	die(failed("allowing to read user <strong>$user</strong> record without signing-in"));
+
+if ($harubi_last_preset_invoked !== FALSE)
+	notice("preset <strong>$harubi_last_preset_invoked</strong> was applied");
+
+success("reading user <strong>$user</strong> record without signing-in failed as expected");
+
+//--------------------------------------------------------
+$module = '../user.php';
+$model  = 'user';
+$action = 'update';
+
+testing(__LINE__, "$model::$action", "without signing-in");
+
+$user		= 'jamal';
+$password	= 'vision1';
+$email		= 'jamal_one@example.com';
+
+echo_br("Updating user <strong>$user</strong> record without signing-in...");
+$results = as_array(request($module, $model, $action, ['user' => $user, 'password' => $password, 'email' => $email]));
+print_pre($results);
+
+if (!with_status($results, ['status' => 0, 'error_code' => 1000]))
+	die(failed("allowing to update user <strong>$user</strong> record without signing-in"));
+
+if ($harubi_last_preset_invoked !== FALSE)
+	notice("preset <strong>$harubi_last_preset_invoked</strong> was applied");
+
+success("updating user <strong>$user</strong> record without signing-in failed as expected");
+
+//--------------------------------------------------------
+$module = '../user.php';
+$model  = 'user';
+$action = 'delete';
+
+testing(__LINE__, "$model::$action", "without signing-in");
+
+$user		= 'jamal';
+
+echo_br("Deleting user <strong>$user</strong> record without signing-in...");
+$results = as_array(request($module, $model, $action, ['user' => $user]));
+print_pre($results);
+
+if (!with_status($results, ['status' => 0, 'error_code' => 1000]))
+	die(failed("allowing to delete user <strong>$user</strong> record without signing-in"));
+
+if ($harubi_last_preset_invoked !== FALSE)
+	notice("preset <strong>$harubi_last_preset_invoked</strong> was applied");
+
+success("deleting user <strong>$user</strong> record without signing-in failed as expected");
+
+//--------------------------------------------------------
+$module = '../user.php';
+$model  = 'user';
+$action = 'signin';
+
+testing(__LINE__, "$model::$action", "existing user");
 
 $user		= 'jamal';
 $password	= 'vision';
 
 echo_br("Signing-in existing user <strong>$user</strong>...");
-$results = as_array(request('../user.php', 'user', 'signin', ['name' => $user, 'password' => $password]));
+$results = as_array(request($module, $model, $action, ['name' => $user, 'password' => $password]));
 print_pre($results);
 
 if (!with_status($results, ['status' => 1]))
@@ -201,13 +493,17 @@ if (!with_status($results, ['status' => 1]))
 success("signed-in existing user <strong>$user</strong>");
 
 //--------------------------------------------------------
-testing(__LINE__, "user::signin super-user");
+$module = '../user.php';
+$model  = 'user';
+$action = 'signin';
+
+testing(__LINE__, "$model::$action", "super-user");
 
 $user		= 'admin';
 $password	= 'secret';
 
 echo_br("Signing-in super-user <strong>$user</strong>...");
-$results = as_array(request('../user.php', 'user', 'signin', ['name' => $user, 'password' => $password]));
+$results = as_array(request($module, $model, $action, ['name' => $user, 'password' => $password]));
 print_pre($results);
 
 if (!with_status($results, ['status' => 1]))
