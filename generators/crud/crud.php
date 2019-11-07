3c0ca46ea54c12c5b3e7fb140e554257510955d9
Abdullah Daud <chelahmy@gmail.com>
2019-11-07 13:14:32 +0800

    Added annotations

--- generators/crud/crud.php
+++ generators/crud/crud.php
@@ -5,8 +5,55 @@
 
 // This is a work in progress.
 
+/**
+* Harubi implementation process:
+*
+* 1. A high-level model is incepted.
+* 2. The model is manifested into a relational design.
+*    A SQL table is considered as an atomic model.
+* 3. A SQL CREATE TABLE script is implemented.
+* 4. Harubi table settings are captured.
+* 5. Harubi model-actions for CRUD are implemented.
+* 
+*/
+
+/*
+* The CRUD generator will read from a SQL CREATE TABLE script to
+* extract table details such as the table name and the coloumns
+* name and type. The column type will be translated into harubi's
+* 'string', 'integer' or 'float' type. The extracted table details
+* are kept in the following format:
+
+	[
+		'table_name' => [
+			'column_name' => 'type'
+		]
+	]
+   
+* The table details will then be analysed for links where the parent-
+* child relationship will be applied. See assign_links().
+*
+* Table convention:
+*
+* 1. Every table must have an 'id' column.
+*
+* 2. A table has a parent when one of its column name contains another table name
+*    appended with '_id'. A table may have multiple parents.
+*
+* 3. A table has a child when any of other tables has a column name with the former
+*    table name appended with '_id'. A table may have many children.
+*
+* 4. Column 'name' must be unique.
+*   
+*/
+
 require_once 'parse_tables.php';
 
+/**
+* When $name_id string ends with '_id' then the 'name' part
+* will be copied to $name, and TRUE will be returned.
+* Otherwise, return FALSE.
+*/
 function split_name_id($name_id, &$name)
 {
 	$name = '';
@@ -26,6 +73,21 @@ function split_name_id($name_id, &$name)
 	return FALSE;			
 }
 
+/**
+* When applicable then assign each table with its parrents and/or children.
+* New items '__parents__' and/or '__children__' with an array of table
+* names will be added to the applicable table. These are known as table links.
+* Parameter $tables contains a list of tables with an associated array of columns.
+*
+* Every table must have an 'id' column.
+*
+* A table has a parent when one of its column name contains another table name appended
+* with '_id'. A table may have multiple parents.
+*
+* A table has a child when any of other tables has a column name with the former
+* table name appended with '_id'. A table may have many children.
+*
+*/
 function assign_links(&$tables)
 {
 	$links = [];
